7aa0947f10e908d6d3d51592163bccd2be5083fe
Daniel <daniel@pop-os.localdomain>
2019-03-27 00:12:24 -0500

    working on stats

--- src/components/TriggerStats.js
+++ src/components/TriggerStats.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { BarChart } from 'react-easy-chart';
 
 class TriggerStats extends Component {
+  state = {
+    habitBar: []
+  };
+
   getSum = (total, num) => {
     return total + num;
   };
 
   render() {
+    console.log('TCL: TriggerStats -> render -> habits', Object.keys(this.props.habits).length);
+    if (Object.keys(this.props.habits).length > 0) {
+      this.state.habitBar = Object.keys(this.props.habits).map(habitKey => {
+        return { x: habitKey, y: Object.values(this.props.habits[habitKey].triggerCounts).reduce(this.getSum, 0) };
+      });
+    }
+
+    // if (habits) {
+    //   habits.reduce(r => {
+    //     return this.state.habitBar.push({ x: r.habit, y: r.triggerCounts.reduce(this.getsum) });
+    //   });
+    // }
+
     return (
       <React.Fragment>
         {Object.keys(this.props.habits).map(habitKey => (
@@ -13,6 +31,7 @@ class TriggerStats extends Component {
             Habit: {this.props.habits[habitKey].habit}: {Object.values(this.props.habits[habitKey].triggerCounts).reduce(this.getSum)}
           </div>
         ))}
+        <BarChart data={this.state.habitBar} />
       </React.Fragment>
     );
   }
