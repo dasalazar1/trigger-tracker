7a05d4bb30fa064327c99b382544c05628ad6fe8
Daniel <daniel@pop-os.localdomain>
2019-03-30 18:30:38 -0500

    working on charts

--- src/components/TriggerStats.js
+++ src/components/TriggerStats.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BarChart } from 'react-easy-chart';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 class TriggerStats extends Component {
   state = {
@@ -14,7 +14,7 @@ class TriggerStats extends Component {
     console.log('TCL: TriggerStats -> getValues -> habits', Object.keys(this.props.habits).length);
     if (Object.keys(this.props.habits).length > 0) {
       return Object.keys(this.props.habits).map(habitKey => {
-        return { x: habitKey, y: Object.values(this.props.habits[habitKey].triggerCounts).reduce(this.getSum, 0) };
+        return { x: this.props.habits[habitKey].habit, y: Object.values(this.props.habits[habitKey].triggerCounts).reduce(this.getSum, 0) };
       });
     } else {
       return [];
@@ -34,9 +34,17 @@ class TriggerStats extends Component {
 
   render() {
     console.log('TCL: TriggerStats -> render -> habits', Object.keys(this.props.habits).length);
+    console.log('TCL: TriggerStats -> render -> getValues', this.getValues());
     return (
       <React.Fragment>
-        <BarChart data={this.getValues()} />
+        <div>
+          <h2>Total number of times a habit has been triggered</h2>
+          <BarChart width={600} height={300} data={this.getValues()} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <Tooltip />
+            <XAxis dataKey="x" />
+            <Bar dataKey="y" fill="#8884d8" />
+          </BarChart>
+        </div>
       </React.Fragment>
     );
   }
